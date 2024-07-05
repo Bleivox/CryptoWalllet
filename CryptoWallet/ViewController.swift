cc83120e8afff383d4e9c36727a9581d471ba36f
Bleivox <nikita-inbox@mail.ru>
2024-07-05 19:05:29 +0300

    Initial Commit

---
+++ CryptoWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoWallet
+//
+//  Created by Никита Долгов on 05.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
